7970d2b83a48ac859afbe8f153b9293e3c8f9eb4
Pegasusor001 <linus.ji.001@gmail.com>
2021-06-23 17:00:46 -0700

    parameterized queries

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+let queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = $1 || 'JUL02'
+  ORDER BY teacher;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values);
